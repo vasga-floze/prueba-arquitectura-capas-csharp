9be4b73ec362685fc864593e030338551bd2124d
vasga-floze <gabriela.floze@gmail.com>
2022-11-10 23:54:20 -0600

    add the Business Logic Layer

---
+++ Proyecto.BL/EmpleadoBL.cs
@@ -0,0 +1,22 @@
+﻿using Proyecto.BE;
+using Proyecto.DA;
+
+namespace Proyecto.BL
+{
+    public class EmpleadoBL
+    {
+        public EmpleadoBE GetEmpleado()
+        {
+            EmpleadoDA dataAccess= new EmpleadoDA();
+            EmpleadoBE empleado = new EmpleadoBE();
+
+            empleado = dataAccess.GetEmpleado();
+
+            //si está nulo que le asigne 17
+            if (!empleado.Edad.HasValue)
+                empleado.Edad = 17;
+
+            return empleado;
+        }
+    }
+}
