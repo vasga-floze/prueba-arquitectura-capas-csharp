d81fe59edb7e33087c53a1eaa1753dc4e3804899
vasga-floze <gabriela.floze@gmail.com>
2022-11-10 23:53:35 -0600

    add the Data Access Layer

---
+++ Proyecto.DA/EmpleadoDA.cs
@@ -0,0 +1,13 @@
+﻿using Proyecto.BE;
+
+namespace Proyecto.DA
+{
+    public class EmpleadoDA
+    {
+        public EmpleadoBE GetEmpleado()
+        { 
+            //Simular acceso a la BD
+            return new EmpleadoBE { Nombre = "Gabriela Flores" }; 
+        }
+    }
+}
